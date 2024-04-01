14834306cf928306eb643edac3daabd6ec58f680
Webb Pinner <webbpinner@gmail.com>
2024-04-01 09:29:06 -0400

    next round of changes

--- bin/build_openrvdas_logger_config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ssh mt@10.23.9.21 "bash /opt/openrvdas/local/soi/build_logger_config.sh"
-
--- utils/restore_db_from_file.sh
+++ utils/restore_db_from_file.sh
@@ -3,6 +3,64 @@
 # Get the current directory
 current_dir=$(pwd)
 
+DATABASE='openvdm'
+USER='openvdmDBUser'
+BACKUP_DIR='/opt/openvdm/database/backups'
+UPDATE_CORE_VARS=0
+
+# Function to display usage information
+usage() {
+    echo "Usage: $0 [-d database] [-u user] [-b backup_dir] password"
+    echo "Options:"
+    echo "  -d, --database    Specify OpenVDM database (default: $DATABASE)"
+    echo "  -u, --user        Specify the database user (default: $USER)"
+    echo "  -b, --backup_dir  Specify the database backup directory (default: ${BACKUP_DIR})"
+    echo "  -c, --core_vars   Update the CoreVars table (Potentially dangerous)"
+    echo "  password          Specify the password for the db user"
+    exit 1
+}
+
+# Parse command line options
+parse_cmd_args() {
+    while [[ $# -gt 1 ]]; do
+        key="$1"
+        case $key in
+            -d|--database)
+                DATABASE="$2"
+                shift 2
+                ;;
+            -u|--user)
+                USER="$2"
+                shift 2
+                ;;
+            -b|--backup_dir)
+                BACKUP_DIR="$2"
+                shift 2
+                ;;
+            -c|--core_vars)
+                UPDATE_CORE_VARS=1
+                shift 1
+                ;;
+            *)
+                # Unknown option
+                usage
+                ;;
+        esac
+    done
+
+    if [[ $# -eq 0 ]]; then
+        usage
+    fi
+    PASSWD=$1
+
+    # Verify backup directory exists
+    if [[ ! -d $BACKUP_DIR ]]; then
+        echo "ERROR: Backup directory: $BACKUP_DIR does not exist"
+        echo
+        usage
+    fi
+}
+
 # Function to handle errors
 handle_error() {
     echo "Error occurred in script at line $1."
@@ -22,24 +80,21 @@ echo "Script directory is: $script_dir"
 install_dir="$(dirname "$script_dir")"
 echo "Install directory is: $install_dir"
 
-cd $install_dir
-
-# Directory containing SQL files
-sql_directory="$install_dir/database/backups"
-
-# MySQL connection parameters
-mysql_database=$1
-mysql_user=$2
-mysql_password=$3
-
 # Function to display menu and prompt user for selection
 select_sql_file() {
-    local files=("$sql_directory"/*.sql)
+    cd $BACKUP_DIR
+    local files=(*.sql)
+    cd $current_dir
+
     local selected_file
 
     echo "Select SQL file to restore:"
-    select filename in "${files[@]}"; do
-        selected_file="$filename"
+    select opt in ${files} "Cancel"; do
+        if [ $opt == "Cancel" ]; then
+            exit 0
+        fi
+
+        selected_file="$opt"
         break
     done
 
@@ -50,29 +105,48 @@ select_sql_file() {
 # Function to restore MySQL database from selected SQL file
 restore_database() {
     local sql_file="$1"
-    read -p "Enter MySQL root password: " -s root_password
-    echo # For newline after password input
+    local ignore_core_vars="$2"
 
-    # Check if the file exists
-    if [ ! -f "$sql_file" ]; then
-        echo "File not found: $sql_file"
-        exit 1
+    temp_file=$(mktemp)
+    cat $BACKUP_DIR/$sql_file > $temp_file
+
+    if [[ $UPDATE_CORE_VARS -eq 1 ]]; then
+
+        echo "ignore CoreVars table"
+        # Tables to exclude from restoration
+        excluded_tables=("OVDM_CoreVars")
+
+        # Exclude the specific table from the SQL file
+        temp2_file=$(mktemp)
+
+        for table in "${excluded_tables[@]}"; do
+            sed -e "/DROP TABLE IF EXISTS \`${table}\`;/d" \
+                -e "/CREATE TABLE \`${table}\`/,/;/d" \
+                -e "/INSERT INTO \`${table}\`/d" \
+                -e "/LOCK TABLES \`${table}\` WRITE;/d" \
+                $temp_file > $temp2_file && mv $temp2_file $temp_file
+        done
     fi
 
+
     # Restore the database
-    mysql -u"$mysql_user" -p"$mysql_password" "$mysql_database" < "$sql_file"
+    echo "mysql -u $USER -p $PASSWD $DATABASE < $temp_file"
+    # mysql -u"$USER" -p"$PASSWD" "$DATABASE" < $temp_file
 
     if [ $? -eq 0 ]; then
         echo "Database restored successfully."
     else
-        echo "Database restore failed."
+        echo "ERROR: Database restore failed."
     fi
+
+    rm $temp_file
 }
 
 # Main function
 main() {
+    parse_cmd_args "$@"
     select_sql_file
 }
 
 # Entry point
-main
+main "$@"
